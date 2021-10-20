cac2904e14132a8f90102e2ee962f53e26740136
timontrettin <timon.trettin@icloud.com>
2021-10-20 16:40:12 +0200

    completed add activity and "branding"

--- src/components/activity.tsx
+++ src/components/activity.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { doc, deleteDoc  } from "firebase/firestore";
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth, db } from '../services/firebase';
 
 const Activity = (data) => {
+
+  const [user, userLoading, userError] = useAuthState(auth);
 
   return (
     <div className="activity">
       <div className="activity-time">
         From
-        <div>{data.activity.start.toDate().toLocaleString()}</div>
+        <div>{data.activity.activity.start.toDate().toLocaleString()}</div>
         until
-        <div>{data.activity.end.toDate().toLocaleString()}</div>
+        <div>{data.activity.activity.end.toDate().toLocaleString()}</div>
       </div>
       <div className="activity-buttons">
-        <button className="button active">Details</button>
-        <button className="button active">Edit</button>
-        <button className="button active">Delete</button>
+        <button className="button active" onClick={() => deleteDoc(doc(db, "users/"+(user && user.uid)+"/activities", data.activity.id))}>Delete</button>
       </div>
     </div>
   )
